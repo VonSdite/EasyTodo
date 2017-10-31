0a98f9cfdfa0a3a0775a40cd1166fda6001b526b
Little Von <122691411@qq.com>
2017-11-01 00:41:12 +0800

    回到最初对完成事项的操作方式

--- app/src/main/java/xyz/wendyltanpcy/easytodo/Adapter/EventsAdapter.java
+++ app/src/main/java/xyz/wendyltanpcy/easytodo/Adapter/EventsAdapter.java
@@ -2,8 +2,10 @@ package xyz.wendyltanpcy.easytodo.Adapter;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.graphics.Paint;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.RecyclerView;
 import android.view.ContextMenu;
 import android.view.LayoutInflater;
@@ -21,6 +23,7 @@ import java.util.List;
 import xyz.wendyltanpcy.easytodo.R;
 import xyz.wendyltanpcy.easytodo.TodoList.EventContentActivity;
 import xyz.wendyltanpcy.easytodo.helper.CheckBoxSample;
+import xyz.wendyltanpcy.easytodo.model.FinishEvent;
 import xyz.wendyltanpcy.easytodo.model.TodoEvent;
 
 
@@ -206,21 +209,53 @@ public class EventsAdapter extends RecyclerView.Adapter<EventsAdapter.ViewHolder
         hd.checkBoxSample.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                TodoEvent todoEvent = mTodoEventList.get(pos);
-                if(todoEvent.isClicked()) {
-                    hd.checkBoxSample.setChecked(false);
-                    hd.eventNameText.setPaintFlags(hd.eventNameText.getPaintFlags() & (~Paint
-                            .STRIKE_THRU_TEXT_FLAG));
-                    todoEvent.setClicked(false);
-                    todoEvent.save();
-                } else {
-                    hd.checkBoxSample.setChecked(true);
-                    hd.eventNameText.setPaintFlags(hd.eventNameText.getPaintFlags() | Paint
-                            .STRIKE_THRU_TEXT_FLAG);
-                    todoEvent.setClicked(true);
-                    todoEvent.save();
-                    Snackbar.make(hd.itemView,"此事件已完成！",Snackbar.LENGTH_SHORT).show();
-                }
+                hd.checkBoxSample.setChecked(true);
+                hd.eventNameText.setPaintFlags(hd.eventNameText.getPaintFlags() | Paint
+                        .STRIKE_THRU_TEXT_FLAG);
+
+
+                final TodoEvent todoEvent = mTodoEventList.get(pos);
+                final AlertDialog.Builder dialog = new AlertDialog.Builder(mContext);
+                dialog.setTitle("确定已经完成这个事件？");
+                dialog.setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                FinishEvent finishEvent = new FinishEvent();
+                                finishEvent.setEventName(todoEvent.getEventName());
+                                finishEvent.setEventFinishDate(todoEvent.getEventDate());
+                                finishEvent.save();
+                                todoEvent.setClicked(true);
+                                todoEvent.delete();
+                                mTodoEventList.remove(pos);
+                                Snackbar.make(hd.itemView,"干得漂亮", Snackbar.LENGTH_SHORT).show();
+                                notifyItemChanged(pos);
+                            }
+                        });
+                dialog.setNegativeButton("CANCEL", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        hd.checkBoxSample.setChecked(false);
+                        hd.eventNameText.setPaintFlags(hd.eventNameText.getPaintFlags() & (~Paint
+                                .STRIKE_THRU_TEXT_FLAG));
+                        todoEvent.setClicked(false);
+                    }
+                });
+                dialog.show();
+
+//                if(todoEvent.isClicked()) {
+//                    hd.checkBoxSample.setChecked(false);
+//                    hd.eventNameText.setPaintFlags(hd.eventNameText.getPaintFlags() & (~Paint
+//                            .STRIKE_THRU_TEXT_FLAG));
+//                    todoEvent.setClicked(false);
+//                    todoEvent.save();
+//                } else {
+//                    hd.checkBoxSample.setChecked(true);
+//                    hd.eventNameText.setPaintFlags(hd.eventNameText.getPaintFlags() | Paint
+//                            .STRIKE_THRU_TEXT_FLAG);
+//                    todoEvent.setClicked(true);
+//                    todoEvent.save();
+//                    Snackbar.make(hd.itemView,"此事件已完成！",Snackbar.LENGTH_SHORT).show();
+//                }
             }
         });
 
